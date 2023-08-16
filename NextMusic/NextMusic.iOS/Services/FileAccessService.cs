a8fd177589f6099cb8a054d19274663b0de8cc06
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2023-08-16 14:56:13 +0100

    :octocat: :gear: Services: Introduce FileAccessService
    - Custom FileAccessService to fetch and populate .mp3 files
    - Uses Platform Specific implementation

---
+++ NextMusic/NextMusic.iOS/Services/FileAccessService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Internal;
+using NextMusic.Services;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using NextMusic.iOS.Services;
+using System.Linq;
+
+[assembly: Dependency(typeof(FileAccessService))]
+namespace NextMusic.iOS.Services
+{
+    public class FileAccessService : IFileAccessService
+    {
+        public async Task<IEnumerable<string>> GetMusicFilesAsync()
+        {
+            var musicFiles = new List<string>();
+
+            try
+            {
+                var folder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                musicFiles.AddRange(Directory.GetFiles(folder, "*.mp3"));
+
+                // Add other audio formats if needed
+
+                return musicFiles;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return Enumerable.Empty<string>();
+            }
+        }
+    }
+}
+
---
+++ NextMusic/NextMusic/Services/IFileAccessService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NextMusic.Services
+{
+	public interface IFileAccessService
+	{
+		Task<IEnumerable<string>> GetMusicFilesAsync(); 
+	}
+}
+
