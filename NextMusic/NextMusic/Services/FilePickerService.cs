0477c980b541e0336d5037a6a22d75a7a66af55d
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2023-08-18 09:54:40 +0100

    :octocat: :gear: Services: Introduce FilePickerService - To Pick Audio Files - Create Custom FileTypes to detect audios as natively the API supports only png, Image and Video

---
+++ NextMusic/NextMusic/Services/FilePickerService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using NextMusic.Models;
+using Xamarin.Essentials;
+
+namespace NextMusic.Services
+{
+	public class FilePickerService: IFilePickerService
+	{
+        public async Task<List<Song>> PickAudioFilesAsync()
+        {
+            try
+            {
+                var audioFileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    {DevicePlatform.iOS, new[] {"public.audio"} },
+                    {DevicePlatform.Android, new[] {"audio/*"} },
+                });
+                var pickedFiles = await FilePicker.PickMultipleAsync(new PickOptions
+                {
+                    FileTypes = audioFileTypes,
+                    PickerTitle = "Pick Audio Files"
+                });
+
+                if (pickedFiles == null) return new List<Song>(); //TODO: Probably add a custom exception if nothing was picked
+                var songs = pickedFiles.Select(file => new Song
+                {
+                    Id = file.FileName,
+                    FilePath = file.FullPath,
+                    Title = Path.GetFileNameWithoutExtension(file.FileName)
+                }).ToList();
+                return songs;
+
+            }catch(Exception ex)
+            {
+                //TODO: Probably handle the exception with a custom exception header
+                return new List<Song>();
+            }
+        }
+    }
+}
+
---
+++ NextMusic/NextMusic/Services/IFilePickerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NextMusic.Models;
+
+namespace NextMusic.Services
+{
+	public interface IFilePickerService
+	{
+		Task<List<Song>> PickAudioFilesAsync();
+	}
+}
+
