05016a5ce80f11f8549f16c0855b4b7aefbf2111
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2023-09-14 08:09:56 +0100

    :hammer: BaseViewModel: Refactor Code and add IsBusy

--- NewsApp/ViewModels/BaseViewModel.cs
+++ NewsApp/ViewModels/BaseViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace NewsApp.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
@@ -26,4 +35,3 @@ namespace NewsApp.ViewModels
         }
     }
 }
-
