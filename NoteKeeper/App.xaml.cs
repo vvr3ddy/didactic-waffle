d3f9be01743d51b086aaacdb7aa90dd2bd1c3ca4
Vishnuvardhan Reddy <vvr-rockstar@bmsit.it.in>
2023-09-14 11:58:24 +0100

    :sparkles: NoteKeeper: Initial App UI
    - Adjust Colors to suit Material UI theming

---
+++ NoteKeeper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NoteKeeper;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ NoteKeeper/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NoteKeeper;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
