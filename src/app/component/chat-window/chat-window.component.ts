45c4f0c303c3e91796513d1c733a0db492cf9340
Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>
2024-01-17 08:13:13 +0000

    chat-window: Add ability to send messages and get bot response
    Signed-off-by: Vishnuvardhan Reddy <vvr-rockstar@bmsit.in>

--- src/app/component/chat-window/chat-window.component.ts
+++ src/app/component/chat-window/chat-window.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Message } from '../../models/message.model';
 
 @Component({
   selector: 'app-chat-window',
@@ -11,13 +12,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class ChatWindowComponent {
 
+  showAreaElement: boolean = true;
+  userInput: string = '';
   exampleQuestions: string[] = [
     'Tell me a fun fact about the Roman Empire',
     'What are some interesting inventions from the Renaissance era?',
     'Can you share a historical event from the Middle Ages?',
     // Add more example questions as needed
   ];
-  
+  chatMessages: Message[] = []; // Array to store chat messages
+
+
   sendMessage(message: string): void {
     // Handle sending the message
   }
@@ -27,5 +32,39 @@ export class ChatWindowComponent {
   // Function to handle button click and populate selected question
   selectQuestion(question: string): void {
     this.selectedQuestion = question;
+  }
+
+  // Function to send a user message
+  sendUserMessage(): void {
+    if (this.selectedQuestion !== null || this.userInput.trim() !== '') {
+      const userMessage: Message = {
+        content: (this.userInput.trim() !== '' ? this.userInput.trim() : this.selectedQuestion ?? ''),
+        sender: 'user',
+        timestamp: new Date(),
+      };
+
+      this.chatMessages.push(userMessage);
+
+      // Clear the user input
+      this.userInput = '';
+
+      // Clear the selected question
+      this.selectedQuestion = null;
+
+      // Simulate bot response after each user message
+      this.simulateBotResponse();
+      this.showAreaElement = false;
+    }
+  }
+
+  // Function to simulate bot response (for demonstration purposes)
+  simulateBotResponse(): void {
+    const botMessage: Message = {
+      content: 'This is a bot response.',
+      sender: 'bot',
+      timestamp: new Date(),
+    };
+
+    this.chatMessages.push(botMessage);
   }
 }
